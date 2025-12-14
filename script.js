485ebe82c3acdeb5db3cb5a89bd862e3413d2f33
vyomramoul <vyomramoul@gmail.com>
2025-12-14 15:54:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const images = [
-  "images/1.png",
-  "images/2.png",
-  "images/3.png",
-  "images/4.png",
-  "images/5.png",
-  "images/6.png"
+  "/vyomramoul/images/1.png",
+  "/vyomramoul/images/2.png",
+  "/vyomramoul/images/3.png",
+  "/vyomramoul/images/4.png",
+  "/vyomramoul/images/5.png",
+  "/vyomramoul/images/6.png"
 ];
 
 const container = document.getElementById("floating-images");
