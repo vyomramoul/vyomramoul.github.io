79d1afdc8953eeb78b93f856874eff1697be0d8b
vyomramoul <vyomramoul@gmail.com>
2025-12-15 12:43:08 +0530

    Update image sources and enhance floating behavior

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const images = [
-  "/vyomramoul/images/1.png",
-  "/vyomramoul/images/2.png",
-  "/vyomramoul/images/3.png",
-  "/vyomramoul/images/4.png",
-  "/vyomramoul/images/5.png",
-  "/vyomramoul/images/6.png"
+  "https://i.postimg.cc/Pxh0b2xB/1.png",
+  "https://i.postimg.cc/wTPP46MQ/2.png",
+  "https://i.postimg.cc/rm8YRsZJ/3.png",
+  "https://i.postimg.cc/6qPHP0Wz/4.png",
+  "https://i.postimg.cc/kgKjjKkB/5.png",
+  "https://i.postimg.cc/T2VZw8k9/6.png"
 ];
 
 const container = document.getElementById("floating-images");
@@ -13,14 +13,16 @@ images.forEach(src => {
   const img = document.createElement("img");
   img.src = src;
 
-  // Debug safety (remove later if you want)
+  // Safety / debug
   img.onerror = () => {
     console.error("Image failed to load:", src);
   };
 
+  // Random position
   img.style.left = Math.random() * 100 + "vw";
   img.style.top = Math.random() * 100 + "vh";
 
+  // Slow floating
   const duration = 30 + Math.random() * 40;
   img.style.animationDuration = duration + "s";
   img.style.animationDelay = (-Math.random() * duration) + "s";
